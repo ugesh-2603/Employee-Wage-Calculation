cc05f3379aca520821d7e8d21cb383171aaa2527
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-05-08 21:16:36 +0530

    Employee_Attendence

--- Employee Wage Calculation/Program.cs
+++ Employee Wage Calculation/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Calculation");
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+
+                Console.WriteLine("Employee is Present");
+
+            }
+
+            else
+
+            {
+
+                Console.WriteLine("Employee is Absent");
+
+            }
         }
     }
 }
