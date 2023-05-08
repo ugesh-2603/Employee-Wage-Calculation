5dfd9a88e349323f7afe9ac14e94aa96f576603f
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-05-08 21:07:07 +0530

    Welcome to Employee Wage Calculation

---
+++ Employee Wage Calculation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Employee_Wage_Calculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Calculation");
+        }
+    }
+}
