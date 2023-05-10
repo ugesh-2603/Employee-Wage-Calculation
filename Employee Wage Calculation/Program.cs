8d280256d15ae3a1d309f13a2f79fcfde67af6ac
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-05-10 07:50:34 +0530

    Calculate PartTime Wage

--- Employee Wage Calculation/Program.cs
+++ Employee Wage Calculation/Program.cs
@@ -5,7 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Calculation");
-            int IS_FULL_TIME = 1;
+
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
 
             int EMP_RATE_PER_HOUR = 20; 
 
@@ -15,9 +17,17 @@
             
             Random random = new Random();
 
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
 
-            if (empCheck == IS_FULL_TIME)
+            if (empCheck == IS_PART_TIME)
+
+            {
+                Console.WriteLine("Employee is Present");
+                empHrs = 4;
+
+            }
+
+            else if (empCheck == IS_FULL_TIME)
 
             {
                 Console.WriteLine("Employee is Present");
