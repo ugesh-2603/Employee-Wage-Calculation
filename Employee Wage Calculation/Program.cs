4327115e8dd8c3f5466f443b3ab56ecf05948da0
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-05-10 07:32:22 +0530

    Calculate Daily Wage

--- Employee Wage Calculation/Program.cs
+++ Employee Wage Calculation/Program.cs
@@ -4,15 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Calculation");
             int IS_FULL_TIME = 1;
+
+            int EMP_RATE_PER_HOUR = 20; 
+
+            int empHrs = 0;
+
+            int empWage = 0; 
+            
             Random random = new Random();
 
             int empCheck = random.Next(0, 2);
+
             if (empCheck == IS_FULL_TIME)
+
             {
 
-                Console.WriteLine("Employee is Present");
+                empHrs = 8;
 
             }
 
@@ -20,9 +28,12 @@
 
             {
 
-                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
 
             }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR; Console.WriteLine("Emp Wage: " + empWage);
+            Console.WriteLine("Emp Wage : " + empWage);
         }
     }
 }
